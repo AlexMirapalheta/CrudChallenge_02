01ace70f3b35a003472b2d61003840ead79acce0
alex.mirapalheta <alex.mirapalheta@compasso.com.br>
2021-10-25 21:29:10 -0300

    feat(repository): new method to search cities New search way cities added allowing find all cities with contain the same reference

--- src/app/repository/cityRepository.js
+++ src/app/repository/cityRepository.js
@@ -4,9 +4,15 @@ const Repository = require('./repository');
 class CityRepository extends Repository {
   constructor() {
     super(CitySchema);
+    this.schema = CitySchema;
   }
   async listAll(payload) {
-    return super.getAll(payload);
+    if (!payload.name) return super.getAll(payload);
+
+    const dbQuery = { $and: [] };
+    dbQuery.$and.push({ name: { $regex: payload.name, $options: 'i' } });
+    delete payload.name;
+    return super.getAll({ ...dbQuery, ...payload });
   }
 }
 
